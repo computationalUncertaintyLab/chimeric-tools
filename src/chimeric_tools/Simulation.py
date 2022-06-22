31d3681c6b033fb4979938ceac8e1cb50a991c76
Abraham Berlin <alb323@lehigh.edu>
2022-06-22 14:48:07 -0500

    Random Sampling of FIPS

--- src/chimeric_tools/Simulation.py
+++ src/chimeric_tools/Simulation.py
@@ -1,11 +1,24 @@
+"""
+berlin
+
+Simulate COVID data
+
+
+"""
 import os.path
-import matplotlib.pyplot as plt
+from tkinter import Y
+from typing import (
+    Dict,
+    Generator,
+    Union,
+)
 import numpy as np
 import pandas as pd
 from arch.typing import ArrayLike1D
-from matplotlib.lines import Line2D
-
+from arch.bootstrap import CircularBlockBootstrap
 from chimeric_tools import Data
+import statsmodels.api as sm
+
 
 DATA_PATH = "./covid_data.feather"
 
@@ -33,12 +46,60 @@ def load_data(path: str) -> pd.DataFrame:
     return pd.read_feather(path)
 
 
+class MODEL:
+    """
+    Just a quick and dirty test model
+    """
+
+    def __init__(self, data: pd.DataFrame):
+        self.data = np.array(data["value"][1:]).reshape(-1,)
+        self.model = sm.tsa.statespace.SARIMAX(self.data, order=(2, 1, 0))
+        self.result = self.model.fit(disp=0)
+        self.preds = self.result.predict()
+        self.residuals = self.result.resid
+
+    def get_preds(self):
+        """
+        Returns the predictions
+        """
+        return self.preds
+
+    def get_residuals(self):
+        """
+        Returns the residuals
+        """
+        return self.residuals
+
+
 class COVID:
     """
     Covid simulation class
     """
 
-    def __init__(self, geo_values: ArrayLike1D) -> None:
+    def __init__(
+        self,
+        geo_values: Union[np.ndarray, Dict[str, float], int, None],
+        seed: Union[None, int, Generator],
+    ) -> None:
+        if check_for_data(DATA_PATH):
+            self.data = load_data(DATA_PATH)
+        else:
+            self.data = pd.DataFrame(
+                columns=["date", "location", "location_name", "value"]
+            )
+
+        if isinstance(geo_values, np.ndarray):
+            self.geo_values = geo_values
+            self.p = None
+        elif isinstance(geo_values, dict):
+            self.geo_values = np.array(list(geo_values.keys()))
+            self.p = np.array(list(geo_values.values()))
+        elif isinstance(geo_values, int):
+            self.geo_values = np.array([geo_values])
+            self.p = None
+        elif geo_values is None:
+            self.geo_values = self.data["location"].unique()
+            self.p = None
         self.geo_values = geo_values
         if check_for_data(DATA_PATH):
             self.data = load_data(DATA_PATH)
@@ -50,7 +111,18 @@ class COVID:
         if missing.size > 0:
             self.update_data(missing)
             save_data(self.data, DATA_PATH)
-        self.data = self.data.loc[self.data["location"].isin(geo_values)]
+
+        if isinstance(seed, Generator):
+            self.generator = seed
+        elif isinstance(seed, (int, np.integer)):
+            self.generator = np.random.default_rng(int(seed))
+        elif seed is None:
+            self.generator = np.random.default_rng()
+        else:
+            raise TypeError(
+                "generator keyword argument must contain a NumPy Generator or "
+                "RandomState instance or an integer when used."
+            )
 
     def find_missing_geo_values(self):
         """
@@ -63,7 +135,7 @@ class COVID:
         """
         Downloads all geo values data that is not in not on file
         """
-        mask = np.array([True if len(_) >= 4 else False for _ in missing_geo_values])
+        mask = np.array([len(_) >= 4 for _ in missing_geo_values])
         county_values = missing_geo_values[mask]
         state_values = missing_geo_values[~mask]
 
@@ -85,69 +157,20 @@ class COVID:
 
         self.data = pd.concat([self.data, county, state])
 
-    def block_boostrap(self, x, block_size: int, overlap: int = 0) -> list:
+    def pick_geo_values(self, reps):
         """
-        Block Bootstrap
-
-        Parameters
-        ----------
-        x : array_like
-        block_size : int
-        overlap : int  when overlap = 0 then this becomes a non-moving block bootstrap
+        Picks the geo values to use
         """
-        # initialize the bootstrap array and the indices
-        blocks = []
-        start = 0
-        end = block_size
-        # if the block is going to go over the length of the array we stop
-        while end <= len(x):
-            blocks.append(x[start:end])
-            start += block_size - overlap
-            end += block_size - overlap
-        n_blocks = len(blocks)
-        overflow_len = len(x) - start
-        d = []
-        # randomly select n_blocks blocks from the list of blocks with replacement
-        for _ in range(int((len(x) - overflow_len) / block_size) + 1):
-            d.append(blocks[np.random.randint(0, n_blocks)])
-        # unpacks and chops end to correct length ( fun code :) )
-        return [x for xs in d for x in xs][: -(block_size - overflow_len)]
+        indices = self.generator.choice(
+            a=len(self.geo_values), size=reps, p=self.p, dtype=np.int64
+        )
+        return self.geo_values[indices]
 
-    def plot_sim(
-        self,
-        actuals,
-        pred,
-        residuals,
-        iterations: int,
-        block_size: int = 5,
-        overlap: int = 0,
-    ) -> None:
-
+    def simulate(self, reps):
         """
-        Plots the actuals, predicted, and residuals
-
-        Parameters:
-        -----------
-        actuals : array_like
-        pred : array_like
-        residuals : array_like
-        iterations : int
-        block_size : int
-        overlap : int
-
+        Simulates the data
         """
+        geo_for_sample = self.pick_geo_values(reps)
 
-        fig = plt.figure(figsize=(12, 8), dpi=150)
-        for _ in range(0, iterations):
-            _d = self.block_boostrap(residuals, block_size, overlap)
-            new = pred + _d
-            plt.plot(new, color="blue", alpha=0.05)
-        plt.plot(actuals, color="black")
-        plt.plot(pred, color="red")
-        colors = ["black", "red", "blue"]
-        lines = [Line2D([0], [0], color=c) for c in colors]
-        labels = ["Actual", "Prediction", "NBB"]
-        plt.legend(lines, labels)
-        plt.xlabel("Model Week")
-        plt.ylabel("Cases")
-        plt.show()
+        for geo_value in geo_for_sample:
+            sub_data = self.data[self.data["location"] == geo_value]
