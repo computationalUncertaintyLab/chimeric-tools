a2f86068405cb60451a1c2c32d815072e78565b7
Abraham Berlin <alb323@lehigh.edu>
2022-07-25 16:01:37 -0400

    Changed Dir for script

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,7 +3,7 @@
 git config --global user.email "git@github.com"
 git config --global user.name "Github Actions CI"
 
-git add src/chimeric_tools/data/covid.csv
+git add src/chimeric_tools/data
 git commit --message "[skip ci] Github Actions build: $GITHUB_RUN_NUMBER"
 git fetch
 git pull --rebase 
