7033f35c55cff5cb37e9dbc7525b287255cb422e
Abraham Berlin <alb323@lehigh.edu>
2022-08-04 14:01:15 -0400

    AR(1) tom fooling

--- src/chimeric_tools/models.py
+++ src/chimeric_tools/models.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
+import stan
 
 def model(data: pd.DataFrame):
     """
@@ -20,8 +21,72 @@ class ARIMA():
     """
 
     def __init__(self, data: pd.DataFrame):
+        self.model_name = "ARIMA"
         self.data = np.array(data["value"])
-        self.model = sm.tsa.statespace.SARIMAX(self.data, order=(2, 1, 0))
-        self.result = self.model.fit(disp=0)
-        self.preds = self.result.predict()
-        self.residuals = self.result.resid
+        self.fit = self.fit()
+        self.preds = self.predict()
+        self.residuals = self.residuals()
+
+    def fit(self):
+        return sm.tsa.statespace.SARIMAX(self.data, order=(2, 1, 0)).fit(disp=0)
+
+    def predict(self):
+        return self.fit.predict()
+
+    def residuals(self):
+        return self.fit.resid
+
+
+class AR1():
+    """
+    LinReg model with Stan
+    """
+
+    def __init__(self, data: np.ndarray, N_tilde: int):
+        self.model_name = "StanLinReg"
+        self.data = data
+        self.N = len(self.data)
+        self.N_tilde = N_tilde
+        self.fit = self.fit()
+
+    def fit(self):
+        stan_code = """
+            data {
+                int<lower=0> N;
+                vector[N] y;
+                int<lower=0> N_tilde;
+            }
+            parameters {
+                real alpha;
+                real beta;
+                real<lower=0> sigma;
+            }
+            model {
+                alpha ~ normal(0, 10);
+                beta ~ normal(0, 10);
+                sigma ~ cauchy(0, 2.5);
+                y[2:N] ~ normal(alpha + beta * y[1:(N-1)], sigma);
+            }
+            generated quantities {
+                vector[N_tilde] y_tilde;
+                real i = y[N];
+                for (n in 1:N_tilde) {
+                    y_tilde[n] = normal_rng(alpha + beta * i, sigma);
+                    i = y_tilde[n];
+                    }
+            }
+        """
+        
+        data = {"N": self.N, 
+                "y": self.data,
+                "N_tilde": self.N_tilde,
+                }
+        model = stan.build(program_code=stan_code, data=data)
+        return model.sample(num_chains = 4, num_samples= 1000)
+
+
+    def predict(self):
+        predictions = self.fit["y_tilde"] # this is coming from the model object
+        return predictions.T
+
+
